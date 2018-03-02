561993eed02acebb347184ca22baab06d06c1a02
zhanglongdream <zhanglong_hn@163.com>
2018-03-02 18:43:11 +0800

    进入退出效果

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+// import swiperInOut from './swiperInOut'
+/**
+ * [a description]
+ * @date   2018-03-02
+ * @author zhanglong
+ * @param  {[type]}   item [description]
+ * @return {[type]}        [description]
+ */
+function a(item){
+  return item
+}
